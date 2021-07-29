650954da66e4bec3f8c8509e47d699a474616528
Gianluca Nogara <gianluca.nogara99@gmail.com>
2021-07-29 11:10:47 +0200

    Fix memory issues

--- notebook/tweets_utils.py
+++ notebook/tweets_utils.py
@@ -77,29 +77,87 @@ def process_data_disinformation(df: pd.DataFrame, lista):
     original = df[df['rt_created_at'].isna() & df['in_reply_to_status_id'].isna()]
     retweet = df[df['rt_created_at'].notna()]
     reply = df[df['in_reply_to_status_id'].notna()]
+    original = original.reset_index(drop=True)
+    retweet = retweet.reset_index(drop=True)
+    reply = reply.reset_index(drop=True)
+    
+    original_tweet = []
+    original_id = []
+    link = []
+    disinform_rt_name = []
+    disinform_rt_id = []
+    rt_name = []
+    rt_id = []
+    link_tweet = []
+    disinform_replied_name = []
+    disinform_replied_id = []
+    rp_name = []
+    rp_id = []
+    
+    res = 0
+    d_total_len = 0
+    d_original_len = 0
+    d_retweet_len = 0
+    d_reply_len = 0
+    
+    for i in original["user_screen_name"]:
+        if(found(i,lista)):
+            d_total_len = d_total_len + 1
+            d_original_len = d_original_len + 1
+            val = list(original["user_screen_name"]).index(i,res)
+            original_tweet.append(i)
+            original_id.append(original["user_id"][val])
+            link.append(original["urls"][val])
+            res = val + 1
+    res = 0
+    for i in retweet["rt_user_screen_name"]:
+        if(found(i,lista)):
+            d_total_len = d_total_len + 1
+            d_retweet_len = d_retweet_len + 1
+            val = list(retweet["rt_user_screen_name"]).index(i,res)
+            disinform_rt_name.append(i)
+            disinform_rt_id.append(retweet["rt_user_id"][val])
+            rt_name.append(retweet["user_id"][val])
+            rt_id.append(retweet["user_screen_name"][val])
+            link_tweet.append(retweet['urls'][val])
+            res = val + 1
+    res = 0
+    for i in reply['in_reply_to_screen_name']:
+        if(found(i,lista)):
+            d_total_len = d_total_len + 1
+            d_reply_len = d_reply_len + 1
+            val = list(reply['in_reply_to_screen_name']).index(i,res)
+            disinform_replied_name.append(i)
+            disinform_replied_id.append(reply['in_reply_to_user_id'][val])
+            rp_name.append(reply['user_screen_name'][val])
+            rp_id.append(reply["user_id"][val])
+            res = val + 1
     return {
         #original
-        'users': df["user_screen_name"], 
-        'ids': df["user_id"],
-#         'data': df["created_at"],
-        'link': df["urls"],        
+        "original_names":original_tweet,
+        'original_ids': original_id,
+#         'data': original["created_at"],
+        'links': link,        
         #retweet
-        "retweet_ids": retweet["user_id"], 
-        "retweet_users": retweet["user_screen_name"],
-        "retweeted_ids": retweet['rt_user_id'], 
-        "retweeted_users": retweet['rt_user_screen_name'],
-        "rt_link" : retweet['urls'],
+        "retweet_ids": rt_id, 
+        "retweet_users": rt_name,
+        "retweeted_ids": disinform_rt_id, 
+        "retweeted_users": disinform_rt_name,
+        "rt_link" : link_tweet,
         #reply
-        "reply_ids": reply['user_id'], 
-        "reply_users": reply['user_screen_name'],
-        "replied_ids": reply['in_reply_to_user_id'], 
-        "replied_users": reply['in_reply_to_screen_name'],
-        "retweet_ids": retweet["user_id"],
+        "reply_ids": rp_id, 
+        "reply_users": rp_name,
+        "replied_ids": disinform_replied_id, 
+        "replied_users": disinform_replied_name,
+        #length
         "total_len" :len(df), 
         "original_len": len(original), 
         "retweet_len": len(retweet), 
-        "reply_len": len(reply)
-
+        "reply_len": len(reply),
+        "d_total_len" :d_total_len, 
+        "d_original_len": d_original_len, 
+        "d_retweet_len": d_retweet_len, 
+        "d_reply_len": d_reply_len
     }
 
 def process_all_data(filename, cols, flag, list_name, chunksize=chunksize, workers=workers):
